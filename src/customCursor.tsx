ceb45f6abb0eb372d9eb995ef075611d05667ffe
Sam Glass <samuelglass@gmail.com>
2024-06-06 10:57:52 -0400

    typescript stuff

--- src/customCursor.tsx
+++ src/customCursor.tsx
@@ -1,12 +1,13 @@
-import React, { useEffect, useRef } from 'react';
+import { useEffect, useRef } from 'react';
 import './index.css'; // Make sure to include your CSS file
 
 const CustomCursor = () => {
   const cursorRef = useRef(null);
 
   useEffect(() => {
-    const handleMouseMove = (e) => {
+    const handleMouseMove = (e: any) => {
       if (cursorRef.current) {
+        // @ts-ignore
         cursorRef.current.style.transform = `translate3d(${e.clientX}px, ${e.clientY}px, 0)`;
       }
     };
